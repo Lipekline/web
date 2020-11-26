e34f279884d9d69581610bca3c53f3f8dbf0f1eb
Luís Felipe Borges Silva <luis.felipe@cosmetologia.local>
2020-11-26 15:31:21 -0300

    ultima comitacao

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter , Switch , Route } from 'react-router-dom'
 
 function Routes(){
     return {
-
-    BrowserRouter
-    <Route>
+      
     
 
       }
 }
 
-export default  Routes;
+export default Routes;
